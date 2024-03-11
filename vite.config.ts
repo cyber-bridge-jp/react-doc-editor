7f598343e960fd65165d170ed995fbfc1ac762e7
aizhigito <82809523+aizhigito@users.noreply.github.com>
2024-03-11 19:26:25 +0900

    change vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,24 @@
-import { defineConfig } from 'vite'
 import path from 'path'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    "process.env.IS_PREACT": process.env.IS_PREACT,
-  },
-  build: {
-    lib: {
-      entry: path.resolve("src", 'DocumentEditor.tsx'),
-      name: 'react-doc-editor',
-      fileName: (format) => `react-doc-editor.${format}.js`
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom', 'katex', 'lexical', '@lexical/react', '@excalidraw/excalidraw'],
-      output: {
-        globals: {
-          react: 'React'
+    build: {
+        lib: {
+            entry: path.resolve("src", 'DocumentEditor.tsx'),
+            name: 'react-doc-editor',
+            formats: ['es', 'cjs', 'umd'],
+            fileName: (format) => `react-doc-editor.${format}.ts`
+        },
+        rollupOptions: {
+            external: ['react', 'react-dom', 'katex', 'lexical', '@lexical/react', '@excalidraw/excalidraw'],
+            output: {
+                globals: {
+                    react: 'React'
+                }
+            }
         }
-      }
-    }
-  },
-  plugins: [react()],
+    },
+    plugins: [react(), dts({insertTypesEntry: true})]
 })

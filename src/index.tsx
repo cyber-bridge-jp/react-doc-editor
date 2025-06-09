f752dd48ed06319b6b2e53124a7d1b0cc7d02bd5
aizhigito <82809523+aizhigito@users.noreply.github.com>
2025-06-09 18:05:44 +0900

    lint

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,8 @@ import React from "react";
 import {createRoot} from 'react-dom/client'
 import App from "./App.tsx";
 
-const root = createRoot(document.getElementById('root') as HTMLElement);
-
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
 )

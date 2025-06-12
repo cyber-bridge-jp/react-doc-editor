b3ed8bccfa37a5a3d9d450be8e5c0e6c179eafa4
aizhigito <82809523+aizhigito@users.noreply.github.com>
2025-06-12 11:48:00 +0900

    fix Cannot read property'ReactCurrentDispatcher'of undefined

--- vite.config.ts
+++ vite.config.ts
@@ -5,23 +5,31 @@ import dts from 'vite-plugin-dts'
 import * as packageJson from './package.json'
 
 export default defineConfig({
-    plugins: [
-        react(),
-        dts({
-            insertTypesEntry: true,
-            include: ['src/package/editor'],
-            outDir: 'dist/types',
-        })
-    ],
-    build: {
-        lib: {
-            entry: path.resolve("src", 'package/editor/index.ts'),
-            name: 'react-doc-editor',
-            formats: ['es', 'umd'],
-            fileName: (format) => `react-doc-editor.${format}.js`
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+      include: ['src/package/editor'],
+      outDir: 'dist/types',
+    })
+  ],
+  build: {
+    lib: {
+      entry: path.resolve("src", 'package/editor/index.ts'),
+      name: 'react-doc-editor',
+      formats: ['es', 'umd'],
+      fileName: (format) => `react-doc-editor.${format}.js`
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.peerDependencies), 'react/jsx-runtime', 'react/jsx-dev-runtime'],
+      output: {
+        globals: {
+          'react-dom': 'ReactDom',
+          react: 'React',
+          'react/jsx-runtime': 'ReactJsxRuntime',
+          'react/jsx-dev-runtime': 'ReactJsxDevRuntime'
         },
-        rollupOptions: {
-            external: [...Object.keys(packageJson.peerDependencies)],
-        }
+      },
     }
+  }
 })
